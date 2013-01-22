ea0ad868e86a6a83e560f016cab96ffb9f8bcfc4
Ionuț Arțăriși <iartarisi@suse.cz>
2013-01-22 15:54:12 +0100

    generalize %setup parsing to support more %setup options

--- test_git_tarballs.py
+++ test_git_tarballs.py
@@ -36,6 +36,45 @@ class TestGitTarballs(unittest.TestCase):
                              ghb.get_commit_from_spec('example_pkg'))
 
 
+class TestGitParseUpdateSpec(unittest.TestCase):
+    def test_normal(self):
+        self.assertEqual(
+            "\nVersion: 14.0\n%setup -n bar\nSource: bar-14.0.tar.gz\n",
+            ghb.parse_update_spec_file(
+            "\nVersion: 13.14\n%setup -n foo\nSource: foo-13.14.tar.gz\n",
+            "14.0", "bar", "bar-14.0.tar.gz"))
+
+    def test_source_zero(self):
+        self.assertEqual(
+            "\nVersion: 14.0\n%setup -n bar\nSource: bar-14.0.tar.gz\n",
+            ghb.parse_update_spec_file(
+            "\nVersion: 13.14\n%setup -n foo\nSource: foo-13.14.tar.gz\n",
+            "14.0", "bar", "bar-14.0.tar.gz"))
+
+    def test_tabs(self):
+        self.assertEqual(
+            "\nVersion:\t14.0\n%setup\t-n bar\nSource:\tbar-14.0.tar.gz\n",
+            ghb.parse_update_spec_file(
+            "\nVersion:\t13.14\n%setup\t-n foo\nSource:\tfoo-13.14.tar.gz\n",
+            "14.0", "bar", "bar-14.0.tar.gz"))
+
+    def test_other_options_in_setup(self):
+        self.assertEqual(
+            "\nVersion:\t14.0\n"
+            "%setup -q -a0 -n bar -f 33\nSource:\tbar-14.0.tar.gz\n",
+            ghb.parse_update_spec_file(
+            "\nVersion:\t13.14\n"
+            "%setup -q -a0 -n foo -f 33\nSource:\tfoo-13.14.tar.gz\n",
+            "14.0", "bar", "bar-14.0.tar.gz"))
+
+    def test_no_n_option_in_setup(self):
+        self.assertEqual(
+            "\nVersion: 14.0\n%setup -q\nSource: bar-14.0.tar.gz\n",
+            ghb.parse_update_spec_file(
+                "\nVersion: 13.14\n%setup -q\nSource: foo-13.14.tar.gz\n",
+                "14.0", "bar", "bar-14.0.tar.gz"))
+
+
 @contextmanager
 def mock_open(contents):
     with mock.patch("__builtin__.open", return_value=io.StringIO(contents)):
